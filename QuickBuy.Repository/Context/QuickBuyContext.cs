83c18f09daf116a2c8b9ee32cc6cc1379ada2f32
fabiomaias@gmail.com <bakksh01>
2020-10-05 13:47:32 -0300

    Add Entity Framework Core and DBContext

---
+++ QuickBuy.Repository/Context/QuickBuyContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using QuickBuy.Domain.Entities;
+using QuickBuy.Domain.Entities.Valuables;
+
+namespace QuickBuy.Repository.Context
+{
+    public class QuickBuyContext: DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
+        public DbSet<FormOfPayment> FormOfPayments { get; set; }
+
+    }
+}
